bf4e92f8de81bb7d92ad2dc27ca62c006ab1ccd3
Noah Orlando <noahorlando@gmail.com>
2017-04-11 23:39:18 -0400

    Python < 2.7.8 Fix
    Fixed an issue with the hashlib dependencies for the pbkdf2_hmac
    function.

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # OSXChromeDecrypt
-Decrypt Google Chrome and Chromium Passwords and Credit Cards on macOS / OS X. No dependencies, quick, fast.
+Decrypt Google Chrome and Chromium Passwords and Credit Cards on macOS / OS X. Works on all versions of Python 2.x - a default install on macOS / OS X, so no dependencies are needed.
 
 These are the passwords and CC's saved via the "Would you like to remember this password" popup when you login to a website.
 
-Great for if you want to export all of your passwords and CC's with one fell swoop, as oppposed to manually selecting each one through Chrome's UI.
+Great for if you want to export all of your passwords and CC's with one fell swoop (it is very quick), as oppposed to manually selecting each one through Chrome's UI.
 
 Also great for forensic analysis, as you can obtain the safe storage key through a variety of methods.
 
@@ -12,7 +12,7 @@ Also great for forensic analysis, as you can obtain the safe storage key through
 1. Look for any encrypted password data stored in ```~/Library/Application Support/Google/Chrome/*/Login Data``` 
 2. Get the decryption key from the keychain WITHOUT having to confirm the users password!
 3. Use this key to decrypt the passwords.
-4. Print out all of the passwords in a user friendly format.
+4. Print out all of the passwords in a neat format.
   
 # Example usage: 
 
@@ -36,3 +36,5 @@ OUTPUT:
 	Card Number: 4567891011121314
 	Expiration Date: 10/2017
 ```
+
+**Note**: Big thanks to @mitsuhiko (https://github.com/mitsuhiko/python-pbkdf2) for a native python pbkdf2_hmac implementation for OSX < 10.11!
--- chrome_passwords.py
+++ chrome_passwords.py
@@ -1,4 +1,32 @@
-import sqlite3, os, binascii, subprocess, base64, hashlib, tempfile, shutil, glob
+import sqlite3, os, binascii, subprocess, base64, operator
+import tempfile, shutil, glob, hmac, struct, itertools
+
+###Big thanks to @mitsuhiko https://github.com/mitsuhiko/python-pbkdf2 for the below function pbkdf2_bin###
+def pbkdf2_bin(hash_fxn, password, salt, iterations, keylen=16):
+    _pack_int = struct.Struct('>I').pack
+    hashfunc = sha1
+    mac = hmac.new(password, None, hashfunc)
+    def _pseudorandom(x, mac=mac):
+        h = mac.copy()
+        h.update(x)
+        return map(ord, h.digest())
+    buf = []
+    for block in xrange(1, -(-keylen // mac.digest_size) + 1):
+        rv = u = _pseudorandom(salt + _pack_int(block))
+        for i in xrange(iterations - 1):
+            u = _pseudorandom(''.join(map(chr, u)))
+            rv = itertools.starmap(operator.xor, itertools.izip(rv, u))
+        buf.extend(rv)
+    return ''.join(map(chr, buf))[:keylen]
+###Big thanks to @mitsuhiko https://github.com/mitsuhiko/python-pbkdf2 for the above function pbkdf2_bin###
+
+try:
+    from hashlib import pbkdf2_hmac
+except ImportError:
+    #python version not available (Python <2.7.8, macOS < 10.11)
+    #use @mitsuhiko's pbkdf2 method
+    pbkdf2_hmac = pbkdf2_bin
+    from hashlib import sha1
 
 login_data_path = '/Users/*/Library/Application Support/Google/Chrome/*/Login Data'
 cc_data_path = '/Users/*/Library/Application Support/Google/Chrome/*/Web Data'
@@ -33,7 +61,7 @@ def chrome_decrypt(encrypted, iv, key): #AES decryption using the PBKDF2 key and
 
 def chrome_process(safe_storage_key, chrome_data):
     iv = ''.join(('20',) * 16) #salt, iterations, iv, size - https://cs.chromium.org/chromium/src/components/os_crypt/os_crypt_mac.mm
-    key = hashlib.pbkdf2_hmac('sha1', safe_storage_key, b'saltysalt', 1003)[:16]
+    key = pbkdf2_hmac('sha1', safe_storage_key, b'saltysalt', 1003)[:16]
     copy_path = tempfile.mkdtemp() #work around for locking DB
     with open(chrome_data, 'r') as content:
         dbcopy = content.read()
@@ -55,13 +83,17 @@ def chrome_process(safe_storage_key, chrome_data):
     shutil.rmtree(copy_path)
     return decrypted_list
 
-for profile in chrome_data:
-    for i, x in enumerate(chrome_process(safe_storage_key, "%s" % profile)):
-    	if 'Web Data' in profile:
-            if i == 0:
-                print "%sCredit Cards for Chrome Profile%s -> [%s%s%s]" % ('\033[92m', '\033[0m', '\033[95m', profile.split('/')[-2], '\033[0m')
-            print "   %s[%s]%s %s%s%s\n\t%sCard Name%s: %s\n\t%sCard Number%s: %s\n\t%sExpiration Date: %s%s/%s" % ('\033[32m', (i+1), '\033[0m', '\033[1m', get_cc(x[2]), '\033[0m', '\033[32m', '\033[0m', x[1], '\033[32m', '\033[0m', x[2], '\033[32m', '\033[0m', x[0], x[3])
-        else:
-            if i == 0:
-                print "%sPasswords for Chrome Profile%s -> [%s%s%s]" % ('\033[92m', '\033[0m', '\033[95m', profile.split('/')[-2], '\033[0m')
-            print "   %s[%s]%s %s%s%s\n\t%sUser%s: %s\n\t%sPass%s: %s" % ('\033[32m', (i + 1), '\033[0m', '\033[1m', x[0], '\033[0m', '\033[32m', '\033[0m', x[1], '\033[32m', '\033[0m', x[2])
+def chrome():
+    for profile in chrome_data:
+        for i, x in enumerate(chrome_process(safe_storage_key, "%s" % profile)):
+            if 'Web Data' in profile:
+                if i == 0:
+                    print "%sCredit Cards for Chrome Profile%s -> [%s%s%s]" % ('\033[92m', '\033[0m', '\033[95m', profile.split('/')[-2], '\033[0m')
+                print "   %s[%s]%s %s%s%s\n\t%sCard Name%s: %s\n\t%sCard Number%s: %s\n\t%sExpiration Date: %s%s/%s" % ('\033[32m', (i+1), '\033[0m', '\033[1m', get_cc(x[2]), '\033[0m', '\033[32m', '\033[0m', x[1], '\033[32m', '\033[0m', x[2], '\033[32m', '\033[0m', x[0], x[3])
+            else:
+                if i == 0:
+                    print "%sPasswords for Chrome Profile%s -> [%s%s%s]" % ('\033[92m', '\033[0m', '\033[95m', profile.split('/')[-2], '\033[0m')
+                print "   %s[%s]%s %s%s%s\n\t%sUser%s: %s\n\t%sPass%s: %s" % ('\033[32m', (i + 1), '\033[0m', '\033[1m', x[0], '\033[0m', '\033[32m', '\033[0m', x[1], '\033[32m', '\033[0m', x[2])
+
+if __name__ == '__main__':
+    chrome()
